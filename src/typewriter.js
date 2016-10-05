f2cac2dc9b59dd8fc80fc24fdf5c217742dcc351
Davide Fossacecchi <d.fossacecchi@cambiomarcia.com>
2016-10-05 12:05:30 +0200

    Separates the injected set of sentences from the consumed one.

--- src/typewriter.js
+++ src/typewriter.js
@@ -15,6 +15,7 @@ angular.module('phox.typewriter', [])
 					var new_line = attrs['newLine'] || false;
 					var humanize = attrs['humanize'] ? (2 * type_delay / 100 * +attrs['humanize']) : 0;
 					var manage_placeholder = attrs['managePlaceholder'] || false;
+					var sentences_separator = angular.copy(scope.sentences);
 
 					var step = 1;
 					var stop = 0;
@@ -33,10 +34,10 @@ angular.module('phox.typewriter', [])
 					}
 
 					start();
-
+					
 					function start(){
 						if(stop === 0){
-							sentence = angular.isArray(scope.sentences) ? getNextSentence() : scope.sentences;
+							sentence = angular.isArray(sentences_separator) ? getNextSentence() : sentences_separator;
 							sentences_shoulder.push(sentence);
 						}
 						$timeout(loop, getLoopDelay());
@@ -48,7 +49,7 @@ angular.module('phox.typewriter', [])
 						if(stop === 0 || stop === sentence.length){
 							if(new_line) stop = 0;
 							else step *= -1
-							if(angular.isArray(scope.sentences)){
+							if(angular.isArray(sentences_separator)){
 								start();
 							}
 							else if(cursor) elem[0].removeChild(cursor)
@@ -63,13 +64,13 @@ angular.module('phox.typewriter', [])
 
 					function getNextSentence(){
 						var index;
-						if(scope.sentences.length === 0){
-							scope.sentences = sentences_shoulder;
+						if(sentences_separator.length === 0){
+							sentences_separator = sentences_shoulder;
 							sentences_shoulder = [];
 						}
 
-						index = randomize ? Math.round( Math.random() * (scope.sentences.length - 1) ) : 0;
-						return scope.sentences.splice(index, 1)[0];
+						index = randomize ? Math.round( Math.random() * (sentences_separator.length - 1) ) : 0;
+						return sentences_separator.splice(index, 1)[0];
 					}
 
 					function getLoopDelay(){
